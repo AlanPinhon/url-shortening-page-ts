b73b0e2b7e1e14c8568892b9638f98a7102474f1
Alan <pinhoneibor32@gmail.com>
2023-09-29 18:46:14 -0600

    chore: types in getShortedLink

--- src/helpers/getShortedLink.ts
+++ src/helpers/getShortedLink.ts
@@ -1,10 +1,16 @@
+export type LinkResponseBody = {
+  result: {
+    full_short_link: string;
+  }
+}
+
 export const getShortedLink = async (url:string) => {
   const urlFetch = `https://api.shrtco.de/v2/shorten?url=${url}`;
 
   const response = await fetch(urlFetch);
-  const data = await response.json();
+  const data:LinkResponseBody = await response.json();
 
   if(!response.ok) throw console.log(data);
 
-  console.log(data); 
+  console.log(data.result.full_short_link); 
 }
