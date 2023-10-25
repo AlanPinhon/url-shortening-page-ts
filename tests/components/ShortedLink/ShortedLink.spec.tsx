ec801628ef1654a48136b7061653cd36a3635fd2
Alan <pinhoneibor32@gmail.com>
2023-10-24 19:16:56 -0600

    test: tests in LocalStorage updated

--- tests/components/ShortedLink/ShortedLink.spec.tsx
+++ tests/components/ShortedLink/ShortedLink.spec.tsx
@@ -1,8 +1,11 @@
+import React from 'react';
 import { rest } from 'msw';
 import { server } from '../../../src/mocks/server';
 import { afterEach, vi } from 'vitest';
 import { getShortedLink } from '../../../src/helpers/getShortedLink';
 import { LinkResponseData } from '../../../src/types/types';
+import { act, fireEvent, render, screen } from '@testing-library/react';
+import { ShortedLink } from '../../../src/components/ShortenLink/ShortedLink';
 
 describe('Tests on <ShortedLink/>', () => {
 
@@ -46,34 +49,57 @@ describe('Tests on <ShortedLink/>', () => {
     expect(response.long_url).toBe(validLink);
   });
 
-  test('should add data to LocalStorage', () => {
-    const linkData: LinkResponseData = {
-      id: "bit.ly/3PQQKNH",
-      link: "https://bit.ly/3PQQKNH",
-      long_url: "https://www.google.com/",
-    }
+  test('should add data to LocalStorage', async () => {
+    const shortenedLinks: LinkResponseData[] = [];
+    const validLink = 'https://www.youtube.com';
 
-    const shortedLinks:LinkResponseData[] = [];
-    shortedLinks.push(linkData);
+    render(<ShortedLink/>);
 
-    localStorage.setItem('links', JSON.stringify(shortedLinks));
-    expect(setItemSpy).toHaveBeenCalledWith('links', JSON.stringify(shortedLinks));
-    expect(setItemSpy).toHaveBeenCalledTimes(1);
+    const linkInput = screen.getByPlaceholderText('Shorten a link here...') as HTMLInputElement;
+    const shortenBtn = screen.getByRole('button');
+
+   await act( async () => {
+    fireEvent.change(linkInput, {target: {value: validLink}});
+    fireEvent.submit(shortenBtn);
+
+    const response = await getShortedLink(validLink);
+    shortenedLinks.push(response);
+
+    localStorage.setItem('links', JSON.stringify(shortenedLinks));
+   });
+
+    expect(setItemSpy).toHaveBeenCalledWith('links', JSON.stringify(shortenedLinks));
   });
 
-  test('should get data from LocalStorage', () => {
-    const linkData: LinkResponseData = {
-      id : "bit.ly/3ZRT7EC",
-      link : "https://bit.ly/3ZRT7EC",
-      long_url : "https://www.youtube.com/"
-    }
+  test('should get data from LocalStorage and show it in the DOM', async() => {
+    const shortenedLinks: LinkResponseData[] = [];
+    const validLink = 'https://www.youtube.com/';
 
-    localStorage.setItem('links', JSON.stringify([linkData]));
-    const storedData = JSON.parse(localStorage.getItem('links')!);
+    render(<ShortedLink/>);
+
+    const linkInput = screen.getByPlaceholderText('Shorten a link here...') as HTMLInputElement;
+    const shortenBtn = screen.getByRole('button');
+
+    await act( async () => {
+     fireEvent.change(linkInput, {target: {value: validLink}});
+     fireEvent.submit(shortenBtn);
+
+     const response = await getShortedLink(validLink);
+     shortenedLinks.push(response);
+
+     localStorage.setItem('links', JSON.stringify(shortenedLinks));
+    });
     
-    expect(storedData).toStrictEqual([linkData])
+    const storedData:LinkResponseData[] = JSON.parse(localStorage.getItem('links')!);
+
+    const shortLink = screen.getByText('https://bit.ly/3ZRT7EC');
+    const originalLink = screen.getByText(validLink);
+
+    expect(storedData).toStrictEqual(shortenedLinks);
     expect(getItemSpy).toHaveBeenCalledWith('links');
-    expect(getItemSpy).toHaveBeenCalledTimes(1);
+
+    expect(shortLink).toBeTruthy();
+    expect(originalLink).toBeTruthy();
   });
 
 });
