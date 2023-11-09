25c5cf5f973586d2c7c5e71c763ec0cf0a24ba23
Alan <pinhoneibor32@gmail.com>
2023-11-09 13:46:21 -0600

    style: styles in Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,34 +3,45 @@ import FacebookLogo from '/assets/images/icon-facebook.svg';
 import TwitterLogo from '/assets/images/icon-twitter.svg';
 import PinterestLogo from '/assets/images/icon-pinterest.svg';
 import InstagramLogo from '/assets/images/icon-instagram.svg';
+import './FooterStyles.css';
 
 export const Footer = () => (
-  <footer id="footer">
+  <footer id="footer" className="footer">
 
-    <div className="links-container">
-      <img className='logo-footer' src={ShortlyLogo} alt="shortly_logo"/>
+    <div className="links-container container">
+      <img className="logo-footer" src={ShortlyLogo} alt="shortly_logo"/>
 
-      <nav className="footer-nav">
-        <p className="footer-nav-section">Features</p>
-        <li><a href="#">Link Shortening</a></li>
-        <li><a href="#">Branded Links</a></li>
-        <li><a href="#">Analytics</a></li>
-      </nav>
+      <div className="nav-links-container">
 
-      <nav className="footer-nav">
-        <p className="footer-nav-section">Resources</p>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Developers</a></li>
-        <li><a href="#">Support</a></li>
-      </nav>
+        <nav className="footer-nav-container">
+          <p className="footer-nav-section">Features</p>
+          <ul>
+            <li><a href="#">Link Shortening</a></li>
+            <li><a href="#">Branded Links</a></li>
+            <li><a href="#">Analytics</a></li>
+          </ul>
+        </nav>
 
-      <nav className="footer-nav">
-        <p className="footer-nav-section">Company</p>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Our Team</a></li>
-        <li><a href="#">Careers</a></li>
-        <li><a href="#">Contact</a></li>                        
-      </nav>
+        <nav className="footer-nav-container">
+          <p className="footer-nav-section">Resources</p>
+          <ul>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Developers</a></li>
+            <li><a href="#">Support</a></li>
+          </ul>
+        </nav>
+
+        <nav className="footer-nav-container">
+          <p className="footer-nav-section">Company</p>
+          <ul>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Our Team</a></li>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>                        
+        </nav>
+
+      </div>
       
       <nav className="footer-social-nav">
         <a className="social-link" href="#">
