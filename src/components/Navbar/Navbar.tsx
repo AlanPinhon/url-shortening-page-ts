563184f4fb3d4e5c3baacb959c254983a7107031
Alan <pinhoneibor32@gmail.com>
2023-11-30 15:42:15 -0600

    style: updating styles in menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import ShortlyLogo from '/assets/images/logo.svg';
 import MenuIcon from '/assets/images/bars-solid.svg';
 import './NavbarStyles.css';
@@ -7,26 +7,24 @@ export const Navbar = () => {
 
   const [isMenuVisible, setMenuVisible] = useState(false);
 
-  const menuRef = useRef<HTMLDivElement>(null);
-
   const openMenu = () => setMenuVisible(true);
   const closeMenu = () => setMenuVisible(false);
   const stopPropagationInMenu = (e: React.MouseEvent) => e.stopPropagation();
 
   return (
     <section className="nav-section container">
-      <nav id="navbar" className="navbar">
+      <div className="menu">
 
         <div className="logo-icon">
           <img src={ShortlyLogo} alt="shortly-logo"/>
         </div>
 
-        <div onClick={openMenu} className="menu-icon">
+        <button onClick={openMenu} className="menu-icon">
           <img src={MenuIcon} alt="menu-icon" />
-        </div>
+        </button>
 
-        <div ref={menuRef} onClick={closeMenu} className={`menu-container ${isMenuVisible ? 'show' : ''}`}>
-          <div onClick={stopPropagationInMenu} className="nav-container">
+        <div onClick={closeMenu} className={`menu-container ${isMenuVisible ? 'show' : ''}`}>
+          <nav onClick={stopPropagationInMenu} className="nav-container">
             <div className="left">  
               <ul>
                 <li><a className='nav-link' href="#">Features</a></li>
@@ -41,10 +39,10 @@ export const Navbar = () => {
               </ul>
               <a className='btn-link nav' href="#">Sing Up</a>
             </div>
-          </div>
+          </nav>
         </div>
     
-      </nav>
+      </div>
     </section>
   )
 }
